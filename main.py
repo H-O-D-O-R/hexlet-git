a895c98c0dcd03995eada15b9718eeae5e2932eb
Egor <egor.volkov.2005.12.03@gmail.com>
2024-08-12 20:40:59 +0300

    display database elements

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def start(message):
     chose_table(message)
 
 
+#Создание шаблона кнопок столов зала
 def make_markup_tables():
     markup = types.ReplyKeyboardMarkup()
 
@@ -42,10 +43,11 @@ def make_markup_tables():
 
     return markup
 @bot.message_handler(commands=['tables'])
+#Выбор стола пользователем
 def chose_table(message):
     bot.send_message(message.chat.id, 'Выбери стол', reply_markup=make_markup_tables())
     bot.register_next_step_handler(message, correct_table)
-
+#Обработка выбранной кнопки
 def correct_table(message):
     text = message.text
 
@@ -62,7 +64,7 @@ def correct_table(message):
     else:
         return chose_table(message)
 
-
+#Создание шаблона кнопок гостей за столом
 def make_markup_guests(number_of_table):
     markup = types.ReplyKeyboardMarkup()
     
@@ -136,11 +138,11 @@ def make_markup_guests(number_of_table):
         markup.row(btn_pass, btn5, btn6, btn_pass, btn_reminder)
     
     return markup
-
+#Выбор гостя пользователем
 def chose_guest(message, number_of_table):
     bot.send_message(message.chat.id, 'Выбери гостя', reply_markup=make_markup_guests(number_of_table))
     bot.register_next_step_handler(message, correct_guest, number_of_table)
-
+#Обработка выбранной кнопки
 def correct_guest(message, number_of_table):
     text = message.text
 
@@ -159,7 +161,7 @@ def correct_guest(message, number_of_table):
     else:
         return chose_guest(message, number_of_table)
 
-
+#Создание шаблона кнопок меню
 def make_markup_order():
     markup = types.ReplyKeyboardMarkup()
 
@@ -184,16 +186,18 @@ def make_markup_order():
     markup.row(btn_guests_for_table, btn_reminder)
 
     return markup
-
+#Выбор меню пользователем
 def chose_order(message, number_of_table, number_of_guest):
-    bot.send_message(message.chat.id, 'Выбери товары', reply_markup=make_markup_order())
+    bot.send_message(message.chat.id, 'Выбери раздел', reply_markup=make_markup_order())
     bot.register_next_step_handler(message, correct_order, number_of_table, number_of_guest)
-
+#Обработка выбранной кнопки
 def correct_order(message, number_of_table, number_of_guest):
     text = message.text
 
-    if text in []:
-        pass
+    if text == 'Меню':
+        return chose_category_menu(message, number_of_table, number_of_guest)
+    elif text == 'Детское меню':
+        return chose_category_child_menu(message, number_of_table, number_of_guest)
     elif text == 'Зал':
         return chose_table(message)
     elif text == 'Мои столы':
@@ -208,6 +212,123 @@ def correct_order(message, number_of_table, number_of_guest):
         bot.send_message(message.chat.id, 'Товар не найден')
         return chose_order(message, number_of_table, number_of_guest)
 
+#Создание шаблона кнопок категорий меню
+def make_markup_categories():
+    markup = types.ReplyKeyboardMarkup()
+
+    btn_breakfast = types.KeyboardButton('Завтраки')
+    btn_snacks = types.KeyboardButton('Закуски')
+    btn_salads = types.KeyboardButton("Салаты")
+    markup.row(btn_breakfast, btn_snacks, btn_salads)
+
+    btn_soups = types.KeyboardButton('Супы')
+    btn_main_dish = types.KeyboardButton("Горячее")
+    btn_pasta = types.KeyboardButton("Паста")
+    markup.row(btn_soups, btn_main_dish, btn_pasta)
+
+    btn_steak = types.KeyboardButton('Стейк')
+    btn_pizza = types.KeyboardButton("Пицца")
+    btn_desserts = types.KeyboardButton("Десерты")
+    markup.row(btn_steak, btn_pizza, btn_desserts)
+
+    btn_tables = types.KeyboardButton('Зал')
+    btn_client_tables = types.KeyboardButton('Мои столы')
+    markup.row(btn_tables, btn_client_tables)
+
+    btn_guests_for_table = types.KeyboardButton('Гости')
+    btn_reminder = types.KeyboardButton('Напоминания')
+    markup.row(btn_guests_for_table, btn_reminder)
+
+    return markup
+#Выбор категории меню пользователем
+def chose_category_menu(message, number_of_table, number_of_guest):
+    bot.send_message(message.chat.id, 'Выбери категорию', reply_markup=make_markup_categories())
+    bot.register_next_step_handler(message, correct_category_menu, number_of_table, number_of_guest)
+#Обработка выбранной кнопки
+def correct_category_menu(message, number_of_table, number_of_guest):
+    text = message.text
+
+    if text in ["Завтраки", "Закуски", "Салаты", "Супы", "Горячее", "Паста", "Стейк", "Десерты", "Пицца"]:
+        return chose_goods( message, number_of_table, number_of_guest, str('основное ' + text).lower() )
+    elif text == 'Зал':
+        return chose_table(message)
+    elif text == 'Мои столы':
+        bot.send_message(message.chat.id, 'Здесь будут написаны твои столы')
+        return chose_order(message, number_of_table, number_of_guest)
+    elif text == 'Напоминания':
+        bot.send_message(message.chat.id, 'Здесь будут написаны твои напоминания')
+        return chose_order(message, number_of_table, number_of_guest)
+    elif text == 'Гости':
+        return chose_guest(message, number_of_table)
+    else:
+        bot.send_message(message.chat.id, 'Товар не найден')
+        return chose_order(message, number_of_table, number_of_guest)
+
+
+#Созданиие шаблона товаров выбранной категории
+def make_markup_goods(categories, number_of_guest):
+    markup = types.ReplyKeyboardMarkup()
+
+    conn = sqlite3.connect('DNK.db')
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT name from goods where category = '{categories}'")
+    goods = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    btns = [[]]
+    for good in goods:
+        if len(btns[-1]) == 2:
+            btns.append([])    
+        btns[-1].append( types.KeyboardButton( good[0] ) )
+
+    for row in btns:
+        markup.row( *row )
+
+    btn_tables = types.KeyboardButton('Зал')
+    btn_client_tables = types.KeyboardButton('Мои столы')
+    markup.row(btn_tables, btn_client_tables)
+
+    btn_guests_for_table = types.KeyboardButton('Гости')
+    btn_chosen_guest = types.KeyboardButton(f'Гость {number_of_guest}')
+    markup.row(btn_guests_for_table, btn_chosen_guest)
+
+    return markup
+#Выбор блюд выбранной категории
+def chose_goods(message, number_of_table, number_of_guest, categories):
+    bot.send_message(message.chat.id, 'Выбери товар', reply_markup=make_markup_goods(categories, number_of_guest))
+    bot.register_next_step_handler(message, correct_goods, number_of_table, number_of_guest, categories)
+#Обработка выбранной кнопки
+def correct_goods(message, number_of_table, number_of_guest, categories):
+    text = message.text
+
+    if text == 'Зал':
+        return chose_table(message)
+    elif text == 'Мои столы':
+        bot.send_message(message.chat.id, 'Здесь будут написаны твои столы')
+        return chose_order(message, number_of_table, number_of_guest)
+    elif text == f'Гость {number_of_guest}':
+        return chose_order(message, number_of_table, number_of_guest)
+    elif text == 'Гости':
+        return chose_guest(message, number_of_table)
+    else:
+        conn = sqlite3.connect('DNK.db')
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT * from goods where name = '{text}'")
+        goods = cur.fetchall()
+
+        cur.close()
+        conn.close()
+        
+        if goods:
+            bot.send_message(message.chat.id, 'Товар найден')
+            return chose_goods(message, number_of_table, number_of_guest, categories)
+        else:
+            bot.send_message(message.chat.id, f'Товар не найден')
+            return chose_order(message, number_of_table, number_of_guest)
 
 #ПОЛУЧЕНИЕ ФОТО
 @bot.message_handler(content_types=['photo'])
